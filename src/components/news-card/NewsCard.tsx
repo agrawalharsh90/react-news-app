9636628157d685b42e184d484e45e0872707a11d
Harsh Agrawal <agrawalharsh90@gmail.com>
2020-08-17 16:04:33 +0530

    card added

--- src/components/news-card/NewsCard.tsx
+++ src/components/news-card/NewsCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {Card, CardContent, CardMedia, Typography} from "@material-ui/core";
+import './NewsCard.css';
 
 interface NewsCardProps {
     id: string,
@@ -9,7 +11,17 @@ interface NewsCardProps {
 };
 
 const NewsCard: React.FC<NewsCardProps> = (props: NewsCardProps) => {
-    return <div></div>
+    return (<Card>
+        <CardMedia image={props.imageUrl}className={"NewsCardSize"}/>
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+                {props.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {props.descripiton}
+            </Typography>
+        </CardContent>
+    </Card>);
 }
 
 export default NewsCard;
